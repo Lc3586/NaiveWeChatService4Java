737795c5988d2e3e63c054ace9c8d191158d2b9f
Lc3586 <2445624023@qq.com>
2023-04-04 17:54:47 +0800

    ~

--- src/main/java/top/lctr/naive/wechat/service/config/ServiceConfig.java
+++ src/main/java/top/lctr/naive/wechat/service/config/ServiceConfig.java
@@ -1,5 +1,6 @@
 package top.lctr.naive.wechat.service.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Component;
 @Primary
 @Component
 @ConfigurationProperties("service")
+@Data
 public class ServiceConfig {
     /**
      * 服务标识
@@ -38,59 +40,4 @@ public class ServiceConfig {
      * 站点资源文件根目录路径
      */
     private String wwwRootDirectory;
-
-    /**
-     * 服务标识
-     */
-    public String getKey() {
-        return key;
-    }
-
-    public void setKey(String key) {
-        this.key = key;
-    }
-
-    /**
-     * 服务名称
-     */
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * 版本号
-     */
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
-    /**
-     * 启用Swagger
-     */
-    public Boolean getEnableSwagger() {
-        return enableSwagger;
-    }
-
-    public void setEnableSwagger(Boolean enableSwagger) {
-        this.enableSwagger = enableSwagger;
-    }
-
-    /**
-     * 站点资源文件根目录路径
-     */
-    public String getWwwRootDirectory() {
-        return wwwRootDirectory;
-    }
-
-    public void setWwwRootDirectory(String wwwRootDirectory) {
-        this.wwwRootDirectory = wwwRootDirectory;
-    }
 }
